cabc52fe1d48729bcabbd97cb3bd047820a379fe
DAYANA <flowerjessi18@gmail.com>
2026-02-22 16:36:19 -0600

    inicio proyecto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Logística_y_transporte.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
