cc790aa5a9faea1054bd94e534b3d417108e3d82
hectorbarillas123 <104667223+hectorbarillas123@users.noreply.github.com>
2026-05-24 17:16:42 -0600

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -30,5 +30,6 @@ namespace Logistica_y_transporte.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+       // hola mundo
     }
 }
