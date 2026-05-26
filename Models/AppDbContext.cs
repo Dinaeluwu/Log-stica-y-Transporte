e367a47f666b8f031c68b15132655e40b88b0024
DAYANA <jfloresg14@miumg.edu.gt>
2026-05-25 23:20:52 -0600

    ultima actualizacion
    subida

--- Models/AppDbContext.cs
+++ Models/AppDbContext.cs
@@ -1,23 +1,14 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+
 namespace Logistica_y_transporte.Models
 {
-<<<<<<< HEAD
-
-    public class AppDbContext :
-=======
-    //es para prueba
-    //segunda prueba
-    public class AppDbContext:
->>>>>>> 316a3b9e938b5013f3b54f52e5462fa9b4183824
-        IdentityDbContext<IdentityUser>
+    public class AppDbContext : IdentityDbContext<IdentityUser>
     {
-        public AppDbContext
-            (DbContextOptions<AppDbContext> options)
+        public AppDbContext(DbContextOptions<AppDbContext> options)
             : base(options)
         {
-
         }
 
         public DbSet<Persona> Personas { get; set; }
